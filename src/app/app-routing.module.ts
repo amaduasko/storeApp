410e127aadf79a2dd955b250de5260b6e98d3754
Amadu Askofare <amadouaskofare@gmail.com>
2019-12-10 19:56:41 +0400

    responsive changes
    updated responsive changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { StoreContentComponent } from '../Components/store-content/store-content.component';
-import { ItemDetailComponent } from '../Components/item-detail/item-detail-component';
-import { CartComponent } from '../Components/cart/cart.component';
+import { StoreContentComponent } from './Components/store-content/store-content.component';
+import { ItemDetailComponent } from './Components/item-detail/item-detail-component';
+import { CartComponent } from './Components/cart/cart.component';
 
 const routes: Routes = [
   { path: '', component: StoreContentComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from '../Containers/app/app.component';
-import { HeaderComponent } from '../Components/header/header.component';
-import { StoreContentComponent } from '../Components/store-content/store-content.component';
+import { AppComponent } from './Containers/app/app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { StoreContentComponent } from './Components/store-content/store-content.component';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ItemDetailComponent } from '../Components/item-detail/item-detail-component';
-import { CartComponent } from '../Components/cart/cart.component';
+import { ItemDetailComponent } from './Components/item-detail/item-detail-component';
+import { CartComponent } from './Components/cart/cart.component';
 
 @NgModule({
   declarations: [
